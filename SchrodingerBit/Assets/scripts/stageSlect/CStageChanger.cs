c785c6958175a774695366db462fd4c504a5b372
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-06-15 09:39:54 +0900

    神の手

---
+++ SchrodingerBit/Assets/scripts/stageSlect/CStageChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CStageChanger : MonoBehaviour {
+
+    public int m_cameraPositionFlag;
+    public static int cameraPositionFlag;
+
+    public void SceneChanger()
+    {
+
+            CFadeManager.Instance.LoadScene("Game", 2.0f);  
+
+    }
+}
