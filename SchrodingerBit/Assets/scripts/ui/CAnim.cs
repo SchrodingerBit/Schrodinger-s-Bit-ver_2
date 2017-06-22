f08585d20c934b5e2a376bb747e1399e7901c5a6
DA\nwuser <fairy11297@gmail.com>
2017-06-22 14:41:29 +0900

    ゲージ　　エフェクト

---
+++ SchrodingerBit/Assets/scripts/ui/CAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAnim : MonoBehaviour {
+
+    [SerializeField]
+
+    void Start()
+    {
+        transform.SetAsFirstSibling();
+    }
+
+    void Update () {
+        Animator anim = GetComponent<Animator>();
+
+        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (animInfo.normalizedTime > 1.0f)
+        {
+            Destroy(gameObject);    
+        }
+    }
+}
---
+++ SchrodingerBit/Assets/scripts/ui/CGauge.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CGauge : MonoBehaviour {
+
+    Slider slider;                  // InspectorのSliderを引っ張る
+    float time;                     // 制限時間
+
+
+    void Start () {
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+
+        time = slider.maxValue; // ゲージの最大値セット
+    }
+
+
+    void Update () {
+        Timegauge();
+        /*
+        // デバッグ用　ループ
+        if(time < slider.minValue)  // 0になったら
+        {
+          time = slider.maxValue;   // 最大値を代入
+        }
+        */
+
+    }
+
+    void Timegauge()
+    {
+        time -= Time.deltaTime;     // 制限時間カウント　最大値から減らす
+
+        slider.value = time;        // timeゲージに値を設定  
+    }
+
+}
